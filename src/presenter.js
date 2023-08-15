99c09a069942af56aed31f41207ae0fcfbbb4a67
libia <libia.roman@ucb.edu.bo>
2023-08-15 13:33:09 +0200

    saludar por genero

--- src/presenter.js
+++ src/presenter.js
@@ -3,10 +3,15 @@ import multiplicar from "./multiplicador";
 
 const name = document.querySelector("#name");
 const form = document.querySelector("#saludar-form");
+const sexo = document.querySelector("#boxGenero");
 const div = document.querySelector("#resp-div");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
   
-  div.innerHTML = "Hola " + name.value;
+  if(sexo.value == "masculino"){
+    div.innerHTML = "Hola Mr " + name.value;
+  }else{
+    div.innerHTML = "Hola Mrs " + name.value;
+  }
 });
