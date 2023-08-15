17fb79f74e90318c3d6af0a83057b934d97182ed
libia <libia.roman@ucb.edu.bo>
2023-08-15 13:41:21 +0200

    saludar por edad

--- src/presenter.js
+++ src/presenter.js
@@ -4,14 +4,23 @@ import multiplicar from "./multiplicador";
 const name = document.querySelector("#name");
 const form = document.querySelector("#saludar-form");
 const sexo = document.querySelector("#boxGenero");
+const edad = document.querySelector("#age");
 const div = document.querySelector("#resp-div");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
   
   if(sexo.value == "masculino"){
-    div.innerHTML = "Hola Mr " + name.value;
+    if(edad.value < 30){
+      div.innerHTML = "Hola joven " + name.value;
+    }else{
+      div.innerHTML = "Hola Mr " + name.value;
+    }
   }else{
-    div.innerHTML = "Hola Mrs " + name.value;
+    if(edad.value < 30){
+      div.innerHTML = "Hola Miss " + name.value;
+    }else{
+      div.innerHTML = "Hola Mrs " + name.value;
+    }
   }
 });
