880af9306a25361cc92722021c4abc55da839b28
libia <libia.roman@ucb.edu.bo>
2023-08-15 13:56:07 +0200

    saludar por hora

--- src/presenter.js
+++ src/presenter.js
@@ -10,17 +10,32 @@ const div = document.querySelector("#resp-div");
 form.addEventListener("submit", (event) => {
   event.preventDefault();
   
+  let fechaActual = new Date();
+  let horaActual = fechaActual.getHours();
+  console.log(horaActual);
+  let saludo = "";
+  if(horaActual >= 0 & horaActual <=4){
+    saludo = "Buenas noches";
+  }if(horaActual >= 5 & horaActual <=13){
+    saludo = "Buen día";
+  }if(horaActual >= 14 & horaActual <=18){
+    saludo = "Buenas tardes";
+  }if(horaActual >= 19 & horaActual <=23){
+    saludo = "Buenas noches";
+  }
+
+
   if(sexo.value == "masculino"){
     if(edad.value < 30){
-      div.innerHTML = "Hola joven " + name.value;
+      div.innerHTML = saludo + " joven " + name.value;
     }else{
-      div.innerHTML = "Hola Mr " + name.value;
+      div.innerHTML = saludo + " Mr " + name.value;
     }
   }else{
     if(edad.value < 30){
-      div.innerHTML = "Hola Miss " + name.value;
+      div.innerHTML = saludo + " Miss " + name.value;
     }else{
-      div.innerHTML = "Hola Mrs " + name.value;
+      div.innerHTML = saludo + " Mrs " + name.value;
     }
   }
 });
